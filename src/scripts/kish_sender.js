bbc102956bcc9941b3b116817355b7edeb67e4be
Articha <tima-1324@mail.ru>
2025-02-19 13:42:00 +0300

    • Added javascript jQuery as dependency • Tried to implement sending mechanism

--- src/scripts/kish_sender.js
+++ src/scripts/kish_sender.js
@@ -1,11 +1,22 @@
-const regex = new RegExp("files\/task\/([0-9]+)\/group\/([0-9]+)");
-
-function check_url() {
-	if (regex.test(window.location.href)) {
-		alert("Task page")
-	} else {
-		alert("Non task page")
-	}
+function task_page(group, task) {
+	const base_url = `https://articha.tplinkdns.com/chrome-extension/${group}/${task}`
+	$.getJSON({
+		url: base_url + '/exists',
+		success: function(json) {
+			if (json.exists) {
+				return;
+			}
+			$.post(base_url, {'body': $('body').prop("outerHTML")})
+		}
+	})
 }
 
-check_url();
+function check_url() {
+	output = regex.exec(window.location.href);
+	const regex = new RegExp("files\/task\/([0-9]+)\/group\/([0-9]+)");
+    if (output) {
+		task_page(output[2], output[1]);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', check_url);
