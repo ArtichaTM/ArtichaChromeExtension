aec5d3bca602475678ddff7dabb3e4be505ce0db
Articha <tima-1324@mail.ru>
2025-02-19 16:11:41 +0300

    Finished sending algorithm

--- src/scripts/kish_sender.js
+++ src/scripts/kish_sender.js
@@ -1,31 +1,31 @@
-async function task_page(group, task) {
+function task_page(group, task) {
 	const base_url = `https://articha.tplinkdns.com/chrome-extension/api/${group}/${task}`
-	var response = await fetch(base_url + '/exists');
-
-	if (await response.json().then(json => json.exists)) { return; }
-
-	fetch(base_url, {
-		method: 'POST',
-		headers: {
-        	'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({
-			"body":  document.getElementsByTagName('body')[0].outerHTML
+	fetch(base_url + '/exists')
+		.then(response => response.json())
+		.then(json => {
+			if (json.exists) return;
+			fetch(base_url, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'text/plain'
+				},
+				body: document.getElementsByTagName('body')[0].outerHTML
+			})
+				.then(r => console.log(`Response: ${r}`))
 		})
-	})
-		.then(r => console.log(`Response: ${r}`))
+	;
 }
 
-async function check_url() {
+function check_url() {
 	const regex = new RegExp("files\/task\/([0-9]+)\/group\/([0-9]+)");
 	output = regex.exec(window.location.href);
     if (output) {
-		await task_page(output[2], output[1]);
+		task_page(output[2], output[1]);
     }
 }
 
 if ( document.readyState === "complete" )  {
-	await check_url();
+	check_url();
 } else {
 	document.addEventListener('DOMContentLoaded', check_url);
 }
